86176ba4ca96e886fde09ef5b7b660a027fa8d49
bb202118 <boris.bordeaux@gmail.com>
2020-11-15 21:08:04 +0100

    Removed uncompatible legacy opengl code

--- netview.cpp
+++ netview.cpp
@@ -14,10 +14,10 @@ void NetView::initializeGL()
 
 void NetView::paintGL()
 {
-    glClear(GL_COLOR);
+    /*glClear(GL_COLOR);
     glBegin(GL_TRIANGLES);
         glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(-0.5f, -0.5f, 0.0f);
         glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(0.0f, 0.5f, 0.0f);
         glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(0.5f, -0.5f, 0.0f);
-    glEnd();
+    glEnd();*/
 }
